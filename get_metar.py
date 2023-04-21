a657ab2c052675e76b62d7c94b96088786983fde
Sung-ho Kim <42758339+lancard@users.noreply.github.com>
2023-04-21 14:50:47 +0900

    Update get_metar.py

--- get_metar.py
+++ get_metar.py
@@ -1,5 +1,6 @@
 import requests
 import time
+import json
 from bs4 import BeautifulSoup
 
 url = "https://global.amo.go.kr/obsMetar/SearchObsMetarList.do?tm=9999.12.31%2023:59"
@@ -8,13 +9,14 @@ html = requests.get(url).text
 soup = BeautifulSoup(html, 'html.parser')
 metar_soup = soup.select("#contentsTb tbody td:-soup-contains(METAR),td:-soup-contains(SPECI)")
 
-metar_map = {}
+metar_map = {} 
 
 for td in metar_soup:
     metar = " ".join(td.get_text().strip().split())
     airport = metar.split(" ")[1]
     if airport == 'COR':
         airport = metar.split(" ")[2]
-    metar_map[airport] = {'metar': metar, 'runway': []}
+    if 'RK' in airport:
+        metar_map[airport] = {'metar': metar, 'runway': []}
 
 print(json.dumps(metar_map))
